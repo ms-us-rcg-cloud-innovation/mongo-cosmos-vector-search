fb250f3404eb09a3161a83f06cd88a8e89e4d174
Arek Bar <83661379+AiGhostMod@users.noreply.github.com>
2024-04-17 16:13:15 -0500

    Add files via upload

---
+++ cosmosVectorSearch.py
@@ -0,0 +1,121 @@
+import os
+from dotenv import load_dotenv
+import requests
+import json
+import pymongo
+
+load_dotenv(override=True)
+
+# ----------------- Set Up Mongo -----------------
+
+# connect to MongoDB
+client = pymongo.MongoClient(os.environ.get("MONGO_DB_CONNECTION_STRING"), tlsAllowInvalidCertificates=True)
+db = client.get_database("test")
+collection = db.get_collection("vectors")
+
+# test connection string
+print(client.list_database_names())
+
+# print all the collections in the database
+print(db.list_collection_names())
+
+# create a new collection if it doesnt exist
+if "vectors" not in db.list_collection_names():
+    db.create_collection("vectors")
+
+collection = db.get_collection("vectors")
+
+create_vector_index = {
+    "createIndexes": "vectors",  # Replace 'vectors' with your actual collection name if different
+    "indexes": [
+        {
+            "name": "vector_index",  # You can customize the index name
+            "keys": {"vector_field": "cosmosSearch"},  # Replace 'vector_field' with your actual vector field
+            "cosmosSearchOptions": {
+                "kind": "vector-ivf",
+                'numLists': 1,
+                "similarity": "COS",  # Or "euclidean" based on your needs
+                "dimensions": 1536  # Set the dimension of your vector
+            }
+        }
+    ]
+}
+
+# # Create the vector index if it doesn't exist
+if "vector_index" not in collection.index_information():
+    collection.create_indexes([pymongo.IndexModel(**create_vector_index['indexes'][0])])
+
+# print all indexes
+print(collection.index_information())
+
+# ----------------- Embedding the Data -----------------
+
+embedding_deployment_url = os.environ.get("AZURE_OPENAI_EMBEDDING_DEPLOYMENT_URL")
+embedding_deployment_name = os.environ.get("AZURE_OPENAI_EMBEDDING_DEPLOYMENT_NAME")
+
+url = f"{embedding_deployment_url}/openai/deployments/{embedding_deployment_name}/embeddings?api-version=2024-02-01"
+
+api_key = os.environ.get("AZURE_OPENAI_API_KEY")
+
+# Set the headers
+headers = {
+    "Content-Type": "application/json",
+    "api-key": api_key
+}
+
+# Define the payload with the input text
+payload = json.dumps({
+    "input": "The food was delicious and the waiter..."
+})
+
+# Make the POST request
+response = requests.post(url, headers=headers, data=payload)
+
+embedding = response.json()["data"][0]["embedding"]
+
+# ----------------- Store In Mongo -----------------
+
+document = {"text": "The food was delicious and the waiter...", "vector_field": embedding}
+collection.insert_one(document)
+print("Stored embedding in MongoDB")
+print(collection.index_information())
+
+# ----------------- Embedding the Query -----------------
+payload2 = json.dumps({
+    "input": "food "
+})
+
+# Make the POST request to get an embedding for the query
+response2 = requests.post(url, headers=headers, data=payload2)
+query_vector = response2.json()["data"][0]["embedding"]
+
+# ----------------- Execute Query Against Mongo -----------------
+
+pipeline = [
+    {
+        "$search": {
+            "index": "vector_index",  # Ensure this matches the name of your vector index
+            "cosmosSearch": {
+                "vector": query_vector,
+                "path": "vector_field",  # The field where the vector is stored
+                "k": 10, # Number of nearest neighbors to retrieve
+                "similarity": "COS"  # Or "euclidean"
+            }
+        }
+    },
+    {
+        "$project": {
+            "_id": 0, 
+            "text": 1,
+            "score": {"$meta": "searchScore"} # return search score
+
+        }
+    }
+]
+
+# Execute the search
+results = collection.aggregate(pipeline)
+
+# Print results
+for document in results:
+    print(document)
